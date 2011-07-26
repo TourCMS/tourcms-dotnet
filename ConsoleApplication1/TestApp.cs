4f84c73fffd63643acb22d9d702e0267b811fefe
Paul Slugocki <paulslugocki@gmail.com>
2011-07-26 15:33:59 +0100

    Tidied up test application

--- ConsoleApplication1/TestApp.cs
+++ ConsoleApplication1/TestApp.cs
@@ -10,26 +10,50 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            #region TestApp Settings
+                //
+                // Tour Operators: 
+                // Set channelId and privateKey, leave marketplaceId as 0
+                //
+                // Partners (Affiliates, Agents etc):
+                // Set marketplaceId and privateKey, leave channelId as 0
+                // optionally set channelId to return just products from  
+                // that connection
+                //
+                // Both:
+                // Optionally set a queryString to filter results
+                // http://www.tourcms.com/support/api/mp/tour_search.php
+                //
+
+                    int channelId = Properties.Settings.Default.channelId;
+                    int marketplaceId = Properties.Settings.Default.marketplaceId;
+                    string privateKey = Properties.Settings.Default.privateKey;
+                
+                    //string queryString = "lat=56.82127&long=-6.09139&k=walking&per_page=3";
+                    string queryString = "";
+                //
+                //
+            #endregion
+
+            // Check settings
+            if( ( channelId == 0 && marketplaceId == 0 ) || privateKey == "" ) 
+            {
+                Console.WriteLine("Warning: You need to enter your API settings for this test app to work!");
+            }
+
             // Create a new TourCMS object
-            marketplaceWrapper myTourCMS = new marketplaceWrapper(Properties.Settings.Default.marketplaceId, Properties.Settings.Default.privateKey);
-
-            int channelId = Properties.Settings.Default.channelId;
-
-            //string queryString = "lat=56.82127&long=-6.09139&k=walking&per_page=3";
-            string queryString = "";
+            marketplaceWrapper myTourCMS = new marketplaceWrapper(marketplaceId, privateKey);
 
             // Query the TourCMS API 
-            XmlDocument doc = myTourCMS.SearchTours("", channelId);
+            XmlDocument doc = myTourCMS.SearchTours(queryString, channelId);
 
             // Check the status, will be OK if there's no problem
             string status = doc.GetElementsByTagName("error")[0].InnerText;
-
-            if (status == "OK")
+            if ( status == "OK" )
             {
-
                 // Display Tour list 
                 XmlNodeList tourList = doc.GetElementsByTagName("tour");
-                foreach (XmlNode tour in tourList)
+                foreach ( XmlNode tour in tourList )
                 {
                     string tourName = tour.SelectSingleNode("tour_name").InnerText;
                     Console.WriteLine(tourName);
@@ -37,8 +61,6 @@ namespace ConsoleApplication1
             } else {
                 Console.WriteLine("Error: " + status);
             }
-
-            
         }
     }
 }
